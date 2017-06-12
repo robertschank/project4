1dc2ff145a00066d081beca8db7c00c3ca22f1b2
bobbyschank <robertschank@gmail.com>
2017-06-12 02:10:13 -0600

    good place to stop with snapshot

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,16 +24,13 @@ import { gameUpdate } from '../actions';
 import { Square } from './Square';
 import { MyCamera } from './MyCamera';
 import MessageItem from './MessageItem';
-import { Card, CardSection, Input } from './common';
+import { Card, CardSection, Confirm, Input } from './common';
 import { containerColor } from '../constants/Colors';
 
 let squaresArray = [];
-// const teamName = '';
-// const gameId = '';
-// const teamName = this.props.teamName;
 
+// Snapshot built in directories for phone storage
 const { CacheDir, DocumentDir, MainBundleDir, MovieDir, MusicDir, PictureDir } = dirs;
-
 
 function SquareObject(index, description) {
   this.index = index;
@@ -51,6 +48,9 @@ class Home extends Component {
     console.log(this.props.gameId);
     // gameId = this.props.gameId;
     // teamName = this.props.teamName;
+
+
+
 
 
     // Hard Coded Descriptions
@@ -98,15 +98,15 @@ class Home extends Component {
       returnedPhotoPath: 'no photo path',
       rowCount: [0, 1, 1, 0],
       colCount: [0, 1, 0, 1],
-          // previewSource: catsSource,
-    error: null,
-    res: null,
-    value: {
-      format: "png",
-      quality: 0.9,
-      result: "file",
-      snapshotContentContainer: false,
-    },
+      // snapshotview stuff
+      error: null,
+      res: null,
+      value: {
+        format: "png",
+        quality: 0.9,
+        result: "file",
+        snapshotContentContainer: false,
+      },
     };
   } // End Constructor
 
@@ -150,7 +150,7 @@ class Home extends Component {
     console.log('HOME.JS componentDidMount. ');
         this.props.messagesGet(this.props.gameId);
     this.sendMessage('TSBot', `Hey people, welcome to Townie Squares! This is a group message area for all teams. We'll send game updates in here too. Remember, this is a game of integrity and honor. It's up to you to match your photos to the given description. Have fun out there!`);
-    this.sendMessage('TSBot', `${this.props.teamName} has joined the game.`);
+    this.sendMessage('TSBot', `${this.props.teamName} joined the game.`);
   }
 
 
@@ -164,15 +164,91 @@ class Home extends Component {
   //   error => console.error("Oops, snapshot failed", error)
   // );
 
-  snapshot = () =>
+  uploadSnapshot = () => {
+  console.log('SNAPSHOT');
   takeSnapshot(this.refs["board"], { path: PictureDir+"/foo.png" })
   .then(
-    uri => console.log("Image saved to", uri),
-    error => console.error("Oops, snapshot failed", error)
-  );
+    uri => this.uploadToStorage(uri), //console.log("Image saved to", uri),
+    error => console.error("Oops, snapshot failed", error),
+  ); // end .then
+  }
+
+  uploadToStorage(x){
+    console.log('uploadToStorage');
+    console.log(x);
+
+    // mediaFile = new File(x + "IMG_" + timeStamp + ".jpg");
 
 
 
+    var file = x;
+
+    // Create the file metadata
+    var metadata = {
+      contentType: 'image/jpeg'
+    };
+
+    // Firebase Storage Stuff
+    // Get a reference to the storage service, which is used to create references in your storage bucket
+    const storage = firebase.storage();
+
+    // Create a storage reference from our storage service
+    const storageRef = storage.ref();
+    const gameStorageRef = storageRef.child(`games/${this.props.gameId}`);
+
+
+
+    // Upload file and metadata to the object 'images/mountains.jpg'
+    var uploadTask = gameStorageRef.child('snapshotview/' + file.name).put(file, metadata);
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+      function(snapshot) {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, function(error) {
+
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+
+        case 'storage/canceled':
+          // User canceled the upload
+          break;
+
+
+        case 'storage/unknown':
+          // Unknown error occurred, inspect error.serverResponse
+          break;
+      }
+    }, function() {
+      // Upload completed successfully, now we can get the download URL
+      var downloadURL = uploadTask.snapshot.downloadURL;
+    });
+
+
+
+
+
+
+
+
+
+
+
+  }
 
   sendMessage = (author, insertMessage) => {
     console.log('sendMessage.');
@@ -208,33 +284,6 @@ class Home extends Component {
 
     this.sendMessage("bing man", `Whoa, ${this.props.teamName} completed a square!`)
     const index = this.state.clickedSquareIndex;
-    // Check For Win:
-    const colMarked = index%4;
-    console.log('COLMARKED' + colMarked);
-    // const x =  
-    console.log('THIS.STATE.COLCOUNT: ' + this.state.colCount);
-    var currentColCount = this.state.colCount;
-    console.log('CURRENTCOLCOUNT: ' + currentColCount);
-    currentColCount[colMarked]++;
-    if (currentColCount[colMarked] >= 4) {
-      console.log('YOU WIN!!!');
-      this.sendMessage("bing man", `Holy moly, ${this.props.teamName} got bingo!!`)
-    }
-    console.log('CURRENTColCount: ' + currentColCount);
-
-    const floorThisThing = index/4;
-    const rowMarked = Math.floor(floorThisThing);
-    console.log('ROWMARKED' + rowMarked);
-    // const x =  
-    console.log('THIS.STATE.ROWCOUNT: ' + this.state.rowCount);
-    var currentRowCount = this.state.rowCount;
-    console.log('CURRENTCOLCOUNT: ' + currentRowCount);
-    currentRowCount[rowMarked]++;
-    if (currentRowCount[rowMarked] >= 4) {
-      console.log('YOU WIN!!!');
-      this.sendMessage("bing man", `Look Out, ${this.props.teamName} got bingo!!`)
-    }
-    console.log('CURRENTColCount: ' + currentRowCount);
 
     // Couldn't get spread operator ... working
 
@@ -250,6 +299,44 @@ class Home extends Component {
       returnedPhotoPath: path,
       squares: newSquares,
     });
+
+    // Check For Win:
+    const colMarked = index%4;
+    console.log('COLMARKED' + colMarked);
+    console.log('THIS.STATE.COLCOUNT: ' + this.state.colCount);
+    var currentColCount = this.state.colCount;
+    console.log('CURRENTCOLCOUNT: ' + currentColCount);
+    currentColCount[colMarked]++;
+    if (currentColCount[colMarked] >= 4) {
+      console.log('YOU WIN!!!');
+      this.sendMessage("bing man", `Lookout! ${this.props.teamName} got bingo!!`)
+
+      // Take a snapshot of the board to send to firebase storage
+      // console.log('SNAPSHOT')
+      // console.log('showCamera: ' + this.state.showCamera)
+      // takeSnapshot(this.refs["board"], { path: PictureDir+"/foo2.png" })
+      //   .then(
+      //     uri => console.log("Image saved to", uri),
+      //     error => console.error("Oops, snapshot failed", error)
+      //   );
+    } // end column win if
+    console.log('CURRENTColCount: ' + currentColCount);
+
+    const floorThisThing = index/4;
+    const rowMarked = Math.floor(floorThisThing);
+    console.log('ROWMARKED' + rowMarked);
+    // const x =  
+    console.log('THIS.STATE.ROWCOUNT: ' + this.state.rowCount);
+    var currentRowCount = this.state.rowCount;
+    console.log('CURRENTCOLCOUNT: ' + currentRowCount);
+    currentRowCount[rowMarked]++;
+    if (currentRowCount[rowMarked] >= 4) {
+      console.log('YOU WIN!!!');
+      this.sendMessage("bing man", `Look Out, ${this.props.teamName} got bingo!!`)
+    } // end row win if
+    console.log('CURRENTColCount: ' + currentRowCount);
+
+
   }; // End takePhoto()
 
   launchCamera = (index) => {
@@ -274,6 +361,10 @@ class Home extends Component {
     return (<MessageItem message={mess} />)
   }
 
+  renderModal() {
+    return (<Confirm />)
+  }
+
   // TODO
   // renderBoard() {
   //   return <Board 
@@ -281,7 +372,7 @@ class Home extends Component {
   
   render() {
     console.log('HOME.js this.state.photoUri: ' + this.state.photoUri);
-
+    console.log('showCamera' + this.state.showCamera)
     return (
       <View style={styles.container}>
       {this.state.showCamera &&
@@ -300,37 +391,40 @@ class Home extends Component {
               Log Out
             </Text>        
           </View>
-          <View style={styles.row} ref="board">
-            {this.renderSquare(this.state.squares[0].index, this.state.squares[0].description, this.state.squares[0].photoPath, this.state.squares[0].marked)} 
-            {this.renderSquare(this.state.squares[1].index, this.state.squares[1].description, this.state.squares[1].photoPath, this.state.squares[1].marked)}
-            {this.renderSquare(this.state.squares[2].index, this.state.squares[2].description, this.state.squares[2].photoPath, this.state.squares[2].marked)}
-            {this.renderSquare(this.state.squares[3].index, this.state.squares[3].description, this.state.squares[3].photoPath, this.state.squares[3].marked)}
-          </View>
-          <View style={styles.row}>
-            {this.renderSquare(this.state.squares[4].index, this.state.squares[4].description, this.state.squares[4].photoPath, this.state.squares[4].marked)}
-            {this.renderSquare(this.state.squares[5].index, this.state.squares[5].description, this.state.squares[5].photoPath, this.state.squares[5].marked)}
-            {this.renderSquare(this.state.squares[6].index, this.state.squares[6].description, this.state.squares[6].photoPath, this.state.squares[6].marked)}
-            {this.renderSquare(this.state.squares[7].index, this.state.squares[7].description, this.state.squares[7].photoPath, this.state.squares[7].marked)}
-          </View>
-          <View style={styles.row}>
-            {this.renderSquare(this.state.squares[8].index, this.state.squares[8].description, this.state.squares[8].photoPath, this.state.squares[8].marked)}
-            {this.renderSquare(this.state.squares[9].index, this.state.squares[9].description, this.state.squares[9].photoPath, this.state.squares[9].marked)}
-            {this.renderSquare(this.state.squares[10].index, this.state.squares[10].description, this.state.squares[10].photoPath, this.state.squares[10].marked)}
-            {this.renderSquare(this.state.squares[11].index, this.state.squares[11].description, this.state.squares[11].photoPath, this.state.squares[11].marked)}
-          </View>
-          <View style={styles.row}>
-            {this.renderSquare(this.state.squares[12].index, this.state.squares[12].description, this.state.squares[12].photoPath, this.state.squares[12].marked)}
-            {this.renderSquare(this.state.squares[13].index, this.state.squares[13].description, this.state.squares[13].photoPath, this.state.squares[13].marked)}
-            {this.renderSquare(this.state.squares[14].index, this.state.squares[14].description, this.state.squares[14].photoPath, this.state.squares[14].marked)}
-            {this.renderSquare(this.state.squares[15].index, this.state.squares[15].description, this.state.squares[15].photoPath, this.state.squares[15].marked)}
+          <View ref="board" style={styles.boardView}>
+            <View style={styles.row}>
+              {this.renderSquare(this.state.squares[0].index, this.state.squares[0].description, this.state.squares[0].photoPath, this.state.squares[0].marked)} 
+              {this.renderSquare(this.state.squares[1].index, this.state.squares[1].description, this.state.squares[1].photoPath, this.state.squares[1].marked)}
+              {this.renderSquare(this.state.squares[2].index, this.state.squares[2].description, this.state.squares[2].photoPath, this.state.squares[2].marked)}
+              {this.renderSquare(this.state.squares[3].index, this.state.squares[3].description, this.state.squares[3].photoPath, this.state.squares[3].marked)}
+            </View>
+            <View style={styles.row}>
+              {this.renderSquare(this.state.squares[4].index, this.state.squares[4].description, this.state.squares[4].photoPath, this.state.squares[4].marked)}
+              {this.renderSquare(this.state.squares[5].index, this.state.squares[5].description, this.state.squares[5].photoPath, this.state.squares[5].marked)}
+              {this.renderSquare(this.state.squares[6].index, this.state.squares[6].description, this.state.squares[6].photoPath, this.state.squares[6].marked)}
+              {this.renderSquare(this.state.squares[7].index, this.state.squares[7].description, this.state.squares[7].photoPath, this.state.squares[7].marked)}
+            </View>
+            <View style={styles.row}>
+              {this.renderSquare(this.state.squares[8].index, this.state.squares[8].description, this.state.squares[8].photoPath, this.state.squares[8].marked)}
+              {this.renderSquare(this.state.squares[9].index, this.state.squares[9].description, this.state.squares[9].photoPath, this.state.squares[9].marked)}
+              {this.renderSquare(this.state.squares[10].index, this.state.squares[10].description, this.state.squares[10].photoPath, this.state.squares[10].marked)}
+              {this.renderSquare(this.state.squares[11].index, this.state.squares[11].description, this.state.squares[11].photoPath, this.state.squares[11].marked)}
+            </View>
+            <View style={styles.row}>
+              {this.renderSquare(this.state.squares[12].index, this.state.squares[12].description, this.state.squares[12].photoPath, this.state.squares[12].marked)}
+              {this.renderSquare(this.state.squares[13].index, this.state.squares[13].description, this.state.squares[13].photoPath, this.state.squares[13].marked)}
+              {this.renderSquare(this.state.squares[14].index, this.state.squares[14].description, this.state.squares[14].photoPath, this.state.squares[14].marked)}
+              {this.renderSquare(this.state.squares[15].index, this.state.squares[15].description, this.state.squares[15].photoPath, this.state.squares[15].marked)}
+            </View>
           </View>
           <ListView style={styles.ListView}
             enableEmptySections
             dataSource={this.dataSource}
             renderRow={this.renderRow}
           />
-          <Text onPress={this.snapshot}>SNAPSHOT!</Text>
+          
           <CardSection style={styles.messageInput} >
+            <Text onPress={this.uploadSnapshot}>XO!</Text>
             <Input
               placeholder="Enter trash talk here."
               label="Group Message"
@@ -339,6 +433,16 @@ class Home extends Component {
             />
             <Text onPress={()=>{this.sendMessage(this.props.teamName, this.state.newMessage)}}>SEND</Text>
           </CardSection>
+     
+
+         {/*} {this.renderModal()} 
+                  <Confirm
+          visible={true}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
+
+       */}
         </View>
       } 
       </View>
@@ -374,11 +478,13 @@ const styles = StyleSheet.create({
     padding: 4,
     color: 'white'
   },
+  boardView: {
+    backgroundColor: 'white',
+  },
   row: {
     flexDirection: 'row',
     margin: 0,
     padding: 0,
-    backgroundColor: 'yellow',
   },
   ListView: {
     flexGrow:1, // Not sure if this is doing anything
