4807eca4a7f42c6904de743eb0df645fd495d382
bobbyschank <robertschank@gmail.com>
2017-12-23 19:53:18 -0700

    leaderboard item styling

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -11,13 +11,13 @@ import _ from 'lodash';
 
 import TabbedNavigator from '../TabbedNavigator';
 import { scoresGet, gameUpdate } from '../actions';
-import { Card, CardSection, Confirm, Header, Input } from './common';
+import { Card, CardSection, Confirm, Button, Header, Input } from './common';
 import { COLOR_PRIMARY_LIGHT } from './styles/commonStyles.js';
 
 const styles = StyleSheet.create({
 		container: {
 			flexDirection: 'column', 
-			justifyContent: 'space-between',
+			justifyContent: 'flex-start',
 			flex: 1,
 		},
     listView: {
@@ -25,6 +25,9 @@ const styles = StyleSheet.create({
     },
     messageInput: {
         backgroundColor: COLOR_PRIMARY_LIGHT,
+    },
+    text: {
+      fontSize: 20,
     },
 });
 
@@ -40,25 +43,13 @@ class ScoreBoard extends Component {
   componentDidMount() {
     console.log('ScoreBoard.JS componentDidMount. ');
     this.props.scoresGet(this.props.gameId);
-    // this.updateScore(); TOOK THIS FUNCTION TO HOME.JS
   }
-
-  // updateScore = () => {
-  //   var updates = {};
-  //   var teamKey = firebase.database().ref(`games/${this.props.gameId}`).push().key;
-
-  //   updates[`games/${this.props.gameId}/teams/${teamKey}`] = 
-  //     {
-  //       teamName: this.props.teamName,
-  //       squaresCompleted: this.state.squaresCompleted,
-  //       rowsCompleted: this.state.rowsCompleted,
-  //     };
-  //   firebase.database().ref().update(updates);
-  // }
 
 	render() {
 
     let teamInfo = this.props.teams.map((team, index)=>{
+        console.log('XKSDJFKSJDVKSJBVKSBVKS', team.teamName)
+        console.log(team.teamName)
       return (
         <View>
           <View style={{flexDirection: 'row', justifyContent: 'space-between', padding: 5,}}>
@@ -71,6 +62,9 @@ class ScoreBoard extends Component {
             <Text>
               {team.rowsCompleted}
             </Text>
+            <Button styleonPress={()=>{}}>
+              Submit
+            </Button>
           </View>
         </View>
       )
@@ -79,22 +73,56 @@ class ScoreBoard extends Component {
 		return (
 			<View style={ styles.container }>
 				<Header headerText={'Leader Board'}/>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between', padding: 5,}}>
+          <Card>
+            {/*<View style={{flexDirection: 'row', justifyContent: 'space-between', padding: 5,}}>  </View>*/}
+              <CardSection style={{justifyContent: 'space-between'}}>
+                <View style={{alignItems: 'center'}}>
+                  <Text style={ styles.text }>
+                    00
+                  </Text>
+                </View>
+                <View style={{flex:3, alignItems: 'flex-start'}}>
+                  <Text style={ styles.text }>
+                    Team Red
+                  </Text>
+                </View>
+                <View style={{flex:1, alignItems: 'center' }}>
+                  <Text style={ styles.text }>
+                    13
+                  </Text>
+                  <Text style={{fontSize: 14}}>
+                    Squares
+                  </Text>
+                </View>
+                <View style={{flex:1, alignItems: 'center' }}>
+                  <Text style={ styles.text }>
+                    2
+                  </Text>
+                  <Text style={{fontSize: 14}}>
+                    Rows
+                  </Text>
+                </View>
+              </CardSection>
 
-            <Text>
-              Team
-            </Text>
-            <Text>
-              Squares
-            </Text>
-            <Text>
-              Rows
-            </Text>
-
-          </View>
+              <CardSection style={{justifyContent: 'space-between'}}>
+                <Text style={ styles.text }>
+                  Hardy Boys
+                </Text>
+                <Text style={ styles.text }>
+                  {}
+                </Text>
+                <Text style={ styles.text }>
+                  Rows
+                </Text>
+                <Text style={ styles.text }>
+                  team
+                </Text>
+              </CardSection>
+          </Card>
           {teamInfo}
-          <Text>HELLO WORLD</Text>
 			</View>
+
+
 		);
 	}
 }
